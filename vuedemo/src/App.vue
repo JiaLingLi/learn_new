523e6d2085d11fcd930b49b671c076d60e91f29b
lijialing <islijialing@gmail.com>
2021-05-04 21:01:35 +0800

    new

---
+++ vuedemo/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+ <div>
+   <input type="text" name="" id="">
+   <button>提交</button>
+   <ul>
+     <li>123</li>
+   </ul>
+ </div>
+</template>
+
+<script>
+
+import { reactive } from 'Vue' 
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+
+</style>
